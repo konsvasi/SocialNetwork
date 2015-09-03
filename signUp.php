03c28cbce7055fed12220ffd4721b605501bd033
konsvasi <konsvass@outlook.com>
2015-09-03 16:44:26 +0300

    Stylish changes on the forms, added signup form

--- signUp.php
+++ signUp.php
@@ -5,7 +5,27 @@
 //check if a user with the same username exists in the database
 //if all checks out I will add the user in the database
 //and redirect the user to his profile
+require_once 'login.php';
 
+$conn = new mysqli($servername, $username, $password, $database);
+
+if(!$conn)
+	die("Connection failed:" . mysqli_connect_error());
+
+
+$myUsername = $_POST['Name'];
+$myPassword = $_POST['Password'];
+$myConfirm = $_POST['conPass'];
+
+//check if the two passwords are the same
+if($myPassword != $myConfirm){
+	print "Your passwords don't match";
+	header("refresh: 5; index.html");
+}
+else{
+	//check username in database
+	print "OK";
+}
 
 
 
