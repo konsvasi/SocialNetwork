a464d6adfdc8e2d30579e34e9a0ad5282918f15a
konsvasi <konsvass@outlook.com>
2015-09-03 16:10:40 +0300

    File upload

---
+++ signUp.php
@@ -0,0 +1,14 @@
+<?php
+//signUp.php
+//Here is where I add a user in my database
+//I validate the input, confirm that the password is written like it should be
+//check if a user with the same username exists in the database
+//if all checks out I will add the user in the database
+//and redirect the user to his profile
+
+
+
+
+
+
+?>
